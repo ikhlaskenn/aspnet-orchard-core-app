3e65fbe4ea000d3eb53bc628528044ea466b67ec
Ikhlass Kennich <ikhlaskenn@users.noreply.github.com>
2026-01-31 20:30:21 +0100

    Add project files.

---
+++ src/Modules/ProjectModule/Manifest.cs
@@ -0,0 +1,10 @@
+using OrchardCore.Modules.Manifest;
+
+[assembly: Module(
+    Name = "ProjectModule",
+    Author = "The Orchard Core Team",
+    Website = "https://orchardcore.net",
+    Version = "0.0.1",
+    Description = "ProjectModule",
+    Category = "Content Management"
+)]
---
+++ src/Modules/ProjectModule/Migrations/Migrations.cs
@@ -0,0 +1,33 @@
+﻿using OrchardCore.ContentManagement.Metadata;
+using OrchardCore.ContentManagement.Metadata.Settings;
+using OrchardCore.ContentManagement.Metadata.Builders;
+using OrchardCore.Data.Migration;
+
+namespace ProjectModule.Migrations;
+
+public class Migrations : DataMigration
+{
+    private readonly IContentDefinitionManager _content;
+    public Migrations(IContentDefinitionManager content) => _content = content;
+
+    public int Create()
+    {
+        // Fully-qualified calls to the extension methods:
+        OrchardCore.ContentManagement.Metadata.ContentDefinitionManagerExtensions
+          .AlterPartDefinitionAsync(_content, "ProjectPart", part => part
+                .WithField("Description", f => f.OfType("TextField").WithDisplayName("Description"))
+                .WithField("Image", f => f.OfType("MediaField").WithDisplayName("Image"))
+                .WithField("ProjectLink", f => f.OfType("LinkField").WithDisplayName("Project Link"))
+            );
+
+        OrchardCore.ContentManagement.Metadata.ContentDefinitionManagerExtensions
+           .AlterTypeDefinitionAsync(_content, "Project", type => type
+                .WithPart("TitlePart")
+                .WithPart("ProjectPart")
+                .Creatable()
+                .Listable()
+            );
+
+        return 1;
+    }
+}
